8591d89135d3d05972aa8595a0dca5edf68cc600
catherineneely <neelc24@wfu.edu>
2025-02-07 13:04:23 -0500

    asks the user to list their favorite artists, and stores this information in an array

--- src/Capstone.java
+++ src/Capstone.java
@@ -1,4 +1,21 @@
 // Catherine Neely
 
+import java.util.*;
+
 public class Capstone {
+    public static void main(String[] args) {
+        String[] userArtists = userMusic();
+
+
+    }
+    public static String[] userMusic() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Who are your favorite music artists? ");
+        String artists = input.nextLine();
+        String[] data = null;
+        while (input.hasNextLine()) {
+            data = artists.split(",");
+        }
+        return data;
+    }
 }
