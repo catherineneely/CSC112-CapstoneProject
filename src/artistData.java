c31167538fb6dad0305e40dd1c58eca8cfcb92f3
catherineneely <neelc24@wfu.edu>
2025-02-07 15:55:46 -0500

    set up another class, and added an explanation for the purpose of the class

---
+++ src/artistData.java
@@ -0,0 +1,6 @@
+// Catherine Neely
+
+public class artistData {
+}
+// this class is where the program will access the data of the artists (such as which
+// artist are connected, who is typically recommended for main artists, etc.) in files
