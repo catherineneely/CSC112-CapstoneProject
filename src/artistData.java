df93803bfd2166b9ac8f32177e3c281c1d855093
catherineneely <neelc24@wfu.edu>
2025-02-07 16:13:21 -0500

    created new class and added explanation for its planned use

--- src/artistData.java
+++ src/artistData.java
@@ -2,5 +2,7 @@
 
 public class artistData {
 }
-// this class is where the program will access the data of the artists (such as which
-// artist are connected, who is typically recommended for main artists, etc.) in files
+// This class is where the program will access the data of the artists (such as which
+// artist are connected, who is typically recommended for main artists, etc.) in files.
+// The main class will call this class after receiving the user's favorite artists and
+// start comparing the names to the names/connections in these files.
