faa9811834fba1dd559ae209ea49fa06f29d6456
catherineneely <neelc24@wfu.edu>
2025-02-07 13:13:25 -0500

    writes the user's artist list to a txt file

--- src/Capstone.java
+++ src/Capstone.java
@@ -1,11 +1,13 @@
 // Catherine Neely
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Capstone {
     public static void main(String[] args) {
         String[] userArtists = userMusic();
-
 
     }
     public static String[] userMusic() {
@@ -17,5 +19,22 @@ public class Capstone {
             data = artists.split(",");
         }
         return data;
+    }
+    public static void userData (String[] userArtists) {
+        FileOutputStream fileOut = null;
+        try {
+            fileOut = new FileOutputStream("userData.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+        PrintWriter pw = new PrintWriter(fileOut);
+        for (int i = 0; i < userArtists.length; i++) {
+            String line = userArtists[i];
+            pw.println(line);
+
+        }
+        pw.flush();
+        pw.close();
     }
 }
