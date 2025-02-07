244dffdc6e4bbaa5ee1d04d6df7008720f2b3719
catherineneely <neelc24@wfu.edu>
2025-02-07 15:45:12 -0500

    removed a method from the main program, to instead put it in a new class

--- src/Capstone.java
+++ src/Capstone.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Capstone {
     public static void main(String[] args) {
         String[] userArtists = userMusic();
-        userData(userArtists);
+        userData.UData(userArtists);
         System.out.println(userArtists[0]);
         System.out.println(userArtists[1]);
         System.out.println(userArtists[2]);
@@ -22,20 +22,5 @@ public class Capstone {
         }
         return data;
     }
-    public static void userData (String[] userArtists) {
-        FileOutputStream fileOut = null;
-        try {
-            fileOut = new FileOutputStream("userData.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found.");
-            System.exit(1);
-        }
-        PrintWriter pw = new PrintWriter(fileOut);
-        for (int i = 0; i < userArtists.length; i++) {
-            String line = userArtists[i];
-            pw.println(line);
-        }
-        pw.flush();
-        pw.close();
-    }
+
 }
