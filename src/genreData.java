cb6f2e99ac5bbb3ac014ef1b739627cb86a69b7f
catherineneely <neelc24@wfu.edu>
2025-02-07 19:48:33 -0500

    added new classes, with their description/purpose in comments

---
+++ src/genreData.java
@@ -0,0 +1,7 @@
+// Catherine Neely
+
+public class genreData {
+}
+// This class is where the program will access the data of music genres (also connected
+// to the artist data) in files. The main class will call this class after receiving
+// the user's desired genres and start connecting the genres to names in the artistData class.
---
+++ src/moodData.java
@@ -0,0 +1,6 @@
+public class moodData {
+}
+// This class is where the program will access the data of user moods in files (while
+// connecting this to their playlist goals). The main class will call this class after receiving
+// the user's mood(s) and use artist and genre data to generate the recommendations. This is
+// also the class where the user can record their activities (gym, work, etc.).
