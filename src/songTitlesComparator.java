9c51108d326089019987dad0fb14bd95115a1834
catherineneely <neelc24@wfu.edu>
2025-03-06 22:30:09 -0500

    comparator for the sort method in the songData object

---
+++ src/songTitlesComparator.java
@@ -0,0 +1,10 @@
+// Catherine Neely
+
+import java.util.Comparator;
+
+public class songTitlesComparator implements Comparator<songData>{
+    @ Override
+    public int compare(songData D1, songData D2) {
+        return Integer.compare(D1.getSongPopularity(), D2.getSongPopularity());
+    }
+}
