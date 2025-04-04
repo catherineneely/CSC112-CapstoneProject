b6acddf348bc2d1296554779eb83aeb2c0b59ecf
catherineneely <neelc24@wfu.edu>
2025-04-04 00:36:32 -0400

    added the constructors, getter/setter methods, and the recommendation method for artist recommendation

--- src/artistData.java
+++ src/artistData.java
@@ -1,8 +1,89 @@
 // Catherine Neely
 
+import java.io.*;
+import java.util.*;
+
 public class artistData {
+    private String artistSong;
+    private String artistAlbum;
+
+    public artistData() {
+        artistSong = "";
+        artistAlbum = "";
+    }
+    public artistData(String artistSong, String artistAlbum) {
+        this.artistSong = artistSong;
+        this.artistAlbum = artistAlbum;
+    }
+
+    public void setArtistSong(String artistSong) {
+        this.artistSong = artistSong;
+    }
+    public void setArtistAlbum(String artistAlbum) {
+        this.artistAlbum = artistAlbum;
+    }
+    public String getArtistSong() {
+        return artistSong;
+    }
+    public String getArtistAlbum() {
+        return artistAlbum;
+    }
+
+    public String getArtists() {
+        FileInputStream fileIn = null;
+        try {
+            fileIn = new FileInputStream("userArtistData.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+        songData songData = new songData();
+        ArrayList<songData> objects = songData.songFileRead();
+        Stack stack = new Stack();
+        Scanner fileScanner = new Scanner(fileIn);
+        int i = 0;
+
+        while(fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            for (int p = 0; p < objects.size(); p++) {
+                if (line.equalsIgnoreCase(objects.get(p).getSongArtist())) {
+                    String genre = objects.get(p).getSongGenre();
+                    Node songGenre = new Node(genre);
+                    stack.push(songGenre);
+                }
+            }
+            i++;
+        }
+
+        Random rand = new Random();
+        int ss = stack.stackSize();
+        int popCount;
+        if (ss > 0) {
+            popCount = rand.nextInt(ss) + 1;
+            for (int j = 0; j < popCount; j++) {
+                Node poppedNode = stack.pop();
+                String poppedGenre = poppedNode.data;
+
+                LinkedList genreArtists = new LinkedList();
+                for (int k = 0; k < objects.size(); k++) {
+                    songData data = objects.get(k);
+                    if (data.getSongGenre().equalsIgnoreCase(poppedGenre)) {
+                        genreArtists.add(data.getSongArtist());
+                    }
+                }
+                if (genreArtists.size() > 0) {
+                    int artistsCount = rand.nextInt(genreArtists.size());
+                    Node curr = genreArtists.head;
+                    for (int w = 0; w < artistsCount; w++) {
+                        curr = curr.next;
+                    }
+                    return curr.data;
+                }
+            }
+        } else {
+            System.out.println("Stack is empty.");
+        }
+        return "No artist recommendation available.";
+    }
+
 }
-// This class is where the program will access the data of the artists (such as which
-// artist are connected, who is typically recommended for main artists, etc.) in files.
-// The main class will call this class after receiving the user's favorite artists and
-// start comparing the names to the names/connections in these files.
